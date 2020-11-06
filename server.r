cfa933e124ee14ab301b6e5f8fa3fd86003fb45a
abhinandan1425 <60711369+abhinandan1425@users.noreply.github.com>
2020-11-06 19:32:50 +0530

    Add files via upload

---
+++ server.r
@@ -0,0 +1,7 @@
+library(shiny nice)
+  
+  output&fit(extremely nice i liked it )
+my roommate is very stupid. he only studys and say his friends that he watch movies.
+he is very cunning.
+})
+print("naughtyyyyyy")
---
+++ ui.r
@@ -0,0 +1,5 @@
+library(shiny)
+{(uri is very nice movie. i enjoyed very much.
+})
+thank you for reviewing me.
+i will never forget your help.
